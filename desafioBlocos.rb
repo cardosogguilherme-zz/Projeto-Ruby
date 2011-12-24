e1e42c0c746d05588c07a7a802db3b5d4108f2a5
Fernando Schuindt <z.schuindt@gmail.com>
2011-12-24 12:16:39 -0300

    Alteração e comentários na resolução do desafio de blocos.

--- desafioBlocos.rb
+++ desafioBlocos.rb
@@ -1,13 +1,29 @@
 # encoding uft-8
+# Era isso mesmo! Massa! :D
+# Uma observação: Não precisava criar dois métodos, era só passar o bloco na hora de chamar.
+# Vou comentar o seu código e mostrar:
+
+=begin  
+  def fazNada (a, b)
+  	yield(a, b)
+  end
+  
+  def juntaInverte (a, b)
+  	fazNada(a, b){|x, y|  puts (x+y).reverse}
+  end
+  
+  #fazNada("oi","ola"){|x, y| puts x+y}
+  
+  juntaInverte("far", "inha")
+=end
 
 def fazNada (a, b)
-	yield(a, b)
+  yield(a, b)
 end
 
-def juntaInverte (a, b)
-	fazNada(a, b){|x, y|  puts (x+y).reverse}
-end
+fazNada("oi","ola") { 
+  |x, y| puts (x+y).reverse
+}
 
-#fazNada("oi","ola"){|x, y| puts x+y}
-
-juntaInverte("far", "inha")
+# Acho que assim ficou mais limpo.
+# O que acha?
